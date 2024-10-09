583d47e4cd138cd215d9e8f9997e931484cb8db9
MikelTelo <mikel_telo@hotmail.com>
2024-10-09 20:46:33 +0200

    final user battleships

--- Hundir la flota/battleships_game/functions.py
+++ Hundir la flota/battleships_game/functions.py
@@ -118,25 +118,25 @@ def player_turn(board_computer, launch_board_user):
     player_wins = False
     while True:
         try:
-            row = random.randint(0, 9)
-            '''
+            #row = random.randint(0, 9)
+            
             row = input('Introduce a row in range 0-9 (or type "exit" to quit): ')
             if row.lower() == 'exit':
                 print('Exiting the game. Goodbye!')
                 return 'exit'  # Return 'exit' if user types "exit"
-            '''
+            
             row = int(row)
             while row < 0 or row > 9:
                 print("Row out of range. Please enter an integer between 0 and 9:")
                 row = int(input('Introduce a row in range 0-9: '))
 
-            column = random.randint(0, 9)
-            '''
+            #column = random.randint(0, 9)
+            
             column = input('Introduce a column in range 0-9 (or type "exit" to quit): ')
             if column.lower() == 'exit':
                 print('Exiting the game. Goodbye!')
                 return 'exit'  # Return 'exit' if user types "exit"
-            '''
+            
             column = int(column)
             while column < 0 or column > 9:
                 print("Column out of range. Please enter an integer between 0 and 9:")
