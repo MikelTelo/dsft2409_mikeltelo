dc2502037375b506f5829c264ade92ec85fff6e9
MikelTelo <mikel_telo@hotmail.com>
2024-10-23 17:30:50 +0200

    marvel

--- 2409_dsft_thebridge_core/2-Data_Analysis/3-Sources/Archivos/Practica/funciones.py
+++ 2409_dsft_thebridge_core/2-Data_Analysis/3-Sources/Archivos/Practica/funciones.py
@@ -1,15 +1,15 @@
-from variables import * 
+from variables import RUTA_DESCARGAS, EXTENSIONES_IMAGENES, EXTENSIONES_DOCUMENTOS, EXTENSIONES_SOFTWARE, EXTENSIONES_OTROS
 
 import os
 import shutil
 
 def ordenar_archivos(ruta_origen):
-    # Definir las extensiones para cada categoría
+    # Definir las extensiones para cada categoría usando las variables importadas
     extensiones = {
-        'Imagenes': ['.jpg', '.jpeg', '.png', '.svg', '.gif'],
-        'Documentos': ['.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pptx'],
-        'Software': ['.exe', '.py','.ipynb'],
-        'Otros': ['.md']
+        'Imagenes': EXTENSIONES_IMAGENES,
+        'Documentos': EXTENSIONES_DOCUMENTOS,
+        'Software': EXTENSIONES_SOFTWARE,
+        'Otros': EXTENSIONES_OTROS
     }
 
     # Crear las carpetas si no existen
@@ -37,3 +37,6 @@ def ordenar_archivos(ruta_origen):
             ruta_destino = os.path.join(ruta_origen, categoria_destino, archivo)
             shutil.move(ruta_archivo, ruta_destino)
             print(f"Movido: {archivo} -> {categoria_destino}")
+
+if __name__ == "__main__":
+    ordenar_archivos(RUTA_DESCARGAS)
--- 2409_dsft_thebridge_core/2-Data_Analysis/3-Sources/Archivos/Practica/variables.py
+++ 2409_dsft_thebridge_core/2-Data_Analysis/3-Sources/Archivos/Practica/variables.py
@@ -1,6 +1,12 @@
-extensiones = {
-        'Imagenes': ['.jpg', '.jpeg', '.png', '.svg', '.gif'],
-        'Documentos': ['.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pptx'],
-        'Software': ['.exe', '.py','.ipynb'],
-        'Otros': ['.md']
-    }
+# variables.py
+
+# Ruta a la carpeta de Descargas o cualquier otra carpeta que desees ordenar
+RUTA_DESCARGAS = r"C:\Users\mikel\OneDrive\Descargas\Actual\TB-DS-BIO-23.09.24\REPOSITORIOS\Mikel\dsft2409_mikeltelo\2409_dsft_thebridge_core\2-Data_Analysis\3-Sources\Archivos\Practica\carpeta_ejercicio\Autocarpeta"
+
+# Otras variables que puedas necesitar
+EXTENSIONES_IMAGENES = ['.jpg', '.jpeg', '.png', '.svg', '.gif']
+EXTENSIONES_DOCUMENTOS = ['.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pptx']
+EXTENSIONES_SOFTWARE = ['.exe', '.py', '.ipynb']
+EXTENSIONES_OTROS = ['.md']
+
+# Puedes agregar más variables según sea necesario
